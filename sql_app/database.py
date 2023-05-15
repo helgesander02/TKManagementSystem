95e2953fe255bdd80bfbc46fad1e066f710cbc1f
Sander <98319298+helgesander02@users.noreply.github.com>
2023-05-15 18:41:07 +0800

    Update database.py

--- sql_app/database.py
+++ sql_app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234@localhost/Databases1"
+SQLALCHEMY_DATABASE_URL = "postgresql://first_data_user:UVe8dmDcPWz3vPgUtAwDodBR9RLaMU0n@dpg-chb32fqk728tp9apj7k0-a.singapore-postgres.render.com/first_data"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
